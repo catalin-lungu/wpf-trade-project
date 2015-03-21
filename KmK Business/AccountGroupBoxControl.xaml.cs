e381143b54f4ca75194ceeaec27facc95941e6c1
catalin-lungu <icatalin.lungu@yahoo.com>
2015-03-21 18:42:21 +0200

    ui beta colmplete

---
+++ KmK Business/AccountGroupBoxControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using KmK_Business.Model;
+using KmK_Business.ViewModel;
+
+namespace KmK_Business
+{
+    /// <summary>
+    /// Interaction logic for AccountGroupBoxControl.xaml
+    /// </summary>
+    public partial class AccountGroupBoxControl : UserControl
+    {
+        AccountGroupBoxControlViewModel vm;
+        public AccountGroupBoxControl(Account account)
+        {
+            InitializeComponent();
+            vm = new AccountGroupBoxControlViewModel(account);
+            this.DataContext = vm;
+        }
+    }
+}
